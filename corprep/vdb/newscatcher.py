dcd2861ee61fcc03fe7c105455c0d8ace0dd7993
John Sukup <john@expectedx.com>
2023-09-13 18:14:01 -1000

    Additional APIs and Newscater Function Dev

--- corprep/vdb/newscatcher.py
+++ corprep/vdb/newscatcher.py
@@ -8,13 +8,26 @@ newscatcher_api = os.getenv('NEWSCATCHER_API')
 
 newscatcherapi = NewsCatcherApiClient(x_api_key=newscatcher_api)
 
+banks = ['Bank of America','Citigroup','Goldman Sachs','JPMorgan Chase','Wells Fargo']
+retailers = ['Walmart','Amazon','Target','Costco','CVS','Walgreens']
+auto = ['General Motors','BMW','Honda','Ford','Volkswagen','Mercedes-Benz','Toyota','Tesla','Hyundai']
+
+def newscatcher_scrape(...)
+
+
+
+
+
+
+
+
 mst_articles = newscatcherapi.get_search_all_pages(q='Goldman Sachs',
                                                    lang='en',
                                                    countries='US',
                                                    page_size=100,
                                                    max_page=10)
 
-file_path = 'C:\mldev\corprep\data\GS.json'
+file_path = 'C:/mldev/corprep/data/GS.json'
 with open(file_path, 'w') as f:
     json.dump(mst_articles, f)
 
