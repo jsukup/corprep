cd46f38a75b747d25b417cb40c49445deebf4c1a
John Sukup <john@expectedx.com>
2023-09-13 18:31:27 -1000

    Added newscatcher_scrape, removed old data files

--- corprep/vdb/newscatcher.py
+++ corprep/vdb/newscatcher.py
@@ -12,22 +12,15 @@ banks = ['Bank of America','Citigroup','Goldman Sachs','JPMorgan Chase','Wells F
 retailers = ['Walmart','Amazon','Target','Costco','CVS','Walgreens']
 auto = ['General Motors','BMW','Honda','Ford','Volkswagen','Mercedes-Benz','Toyota','Tesla','Hyundai']
 
-def newscatcher_scrape(...)
-
-
-
-
-
-
-
-
-mst_articles = newscatcherapi.get_search_all_pages(q='Goldman Sachs',
+def newscatcher_scrape(queries: list[str]):
+    for query in queries:
+        mst_articles = newscatcherapi.get_search_all_pages(q=query,
                                                    lang='en',
                                                    countries='US',
-                                                   page_size=100,
-                                                   max_page=10)
-
-file_path = 'C:/mldev/corprep/data/GS.json'
-with open(file_path, 'w') as f:
-    json.dump(mst_articles, f)
-
+                                                   page_size=1,
+                                                   max_page=1)
+        file_path = f'C:/mldev/corprep/data/{query}.json'
+        with open(file_path, 'w') as f:
+            json.dump(mst_articles, f)
+            
+newscatcher_scrape(banks)
